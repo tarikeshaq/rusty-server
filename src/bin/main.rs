2589b522b7452e2bbfbf82f18728025911f52b01
Tarik <tarikishaq@hotmail.com>
2019-06-28 08:15:56 -0700

    Implement thread pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,13 +4,14 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
+use server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
