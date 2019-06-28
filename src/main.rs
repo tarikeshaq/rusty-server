d7854de85c1986b325aea64af9ad99ced35d8d75
Tarik <tarikishaq@hotmail.com>
2019-06-28 06:45:15 -0700

    basic addition of multiple threads

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 
     println!("Hello, world!");
@@ -20,10 +24,13 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     let (status_line, filepath) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n{}", "test.html")
-        
+        ("HTTP/1.1 200 OK\r\n\r\n", "test.html")
+
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "test.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "notfound.html")
     };
